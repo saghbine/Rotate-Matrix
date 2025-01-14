436df846ddf41d0038471c805724e8f2d4e5fd00
Daniel Saghbini <134181843+saghbine@users.noreply.github.com>
2025-01-14 11:20:14 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,49 +1,190 @@
 //
 //  main.cpp
-//  String Rotation
+//  Rotate Matrix
 //
-//  Created by daniel saghbine on 1/9/25.
-//
-//  CTCI 1.9 String Rotation: Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring (e.g., "waterbottle" is a rotation of "erbottlewat").
-//  Auxiliary space complexity: O(1). Time complexity: O(n).
+//  Created by daniel saghbine on 12/17/24.
+//  CTCI 1.7 Rotate Matrix
 
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
-bool String_Rotation(const string &s1, const string &s2);
+const int N=3;
+
+void Rotate_Matrix(const int m[][N]);
 
 int main()
 {
-    string str1, str2;
+    srand(int(time(0)));
+    
+    int matrix[N][N];
+    
+    cout<<"problem\n-------\n\tGiven an image represented by an N x N matrix, where each pixel in the image is represented by an integer, write a method to rotate the image by 90 degrees. Can you do this in place?\n\n";
     
     cout<<"input\n-----\n";
-    cout<<"\tType a string to serve as the rotation base: ";
-    cin>>str1;
-    cout<<"\tType another string to check if it's a rotation of the base: ";
-    cin>>str2;
+    
+    for(int c=0; c < N; c++)
+    {
+        cout<<'\t';
+        
+        for(int r=0; r < N; r++)
+        {
+            matrix[r][c]=rand()%10;
+            cout<<matrix[r][c]<<' ';
+        }
+        
+        cout<<endl;
+    }
     
     cout<<"\noutput\n------\n";
-    cout<<"\t\""<<str2<<"\" is"<<(String_Rotation(str1, str2) ? "" : "n't")<<" a rotation of \""<<str1<<"\".\n\n";
+    
+    Rotate_Matrix(matrix);
+    Rotate_Matrix(matrix);
+    Rotate_Matrix(matrix);
+    Rotate_Matrix(matrix);
+    
+    cout<<"\nsolution\n--------\n\tAuxiliary space complexity: O(1); time complexity: O(n^2).\n\n";
     
     return 0;
 }
 
-bool String_Rotation(const string &s1, const string &s2) // s1 is the first C++ string and s2 is the second
+bool outie(int c, int n1)
 {
-    if(s1.size() != s2.size()) // check if s1 and s2 don't have the same size
-        return false; // s2 isn't a rotation of s1
-    
-    int i1, i2; // i1 is the index tracker of s1 and i2 is the index tracker of s2
-    
-    for(i2=0; i2 < s2.size() && s2[i2] != s1[0]; i2++); // move i2 to the index where it contains the same char as index 0 of s1 if it does
-    
-    if(s2[i2++] == s1[0]) // this is if it does
+    switch(c)
     {
-        for(i1=1; i1 < s1.size() && i2 < s2.size() && s1[i1] == s2[i2]; i1++, i2++); // move i1 to the first index where there is a char mismatch between it and i2 as it moves too
+        case 1: return n1 < N;
+        default: return n1 >= 0;
+    }
+}
+
+void outer(int c, int &n1)
+{
+    switch(c)
+    {
+        case 1: n1++;
+            break;
+        default: n1--;
+    }
+}
+
+int inn(int c)
+{
+    switch(c)
+    {
+        case 3: return 0;
+        default: return N-1;
+    }
+}
+
+bool innie(int c, int n2)
+{
+    switch(c)
+    {
+        case 3: return n2 < N;
+        default: return n2 >= 0;
+    }
+}
+
+void innermost(int c, int &n2)
+{
+    switch(c)
+    {
+        case 3: n2++;
+            break;
+        default: n2--;
+    }
+}
+
+void print_matrix(const int m[][N])
+{
+    for(int c=0; c < N; c++)
+    {
+        cout<<"\t\t";
         
-        if(s2.find(s1.substr(i1, s1.size()-i1+1)) == -1) // if the final portion of s1 isn't the same as the beginning portion of s2
-            return false; // s2 isn't a rotation of s1
+        for(int r=0; r < N; r++)
+            cout<<m[r][c]<<' ';
+        
+        cout<<endl;
+    }
+}
+
+void Rotate_Matrix(const int m[][N]) // m is input matrix
+{
+    static int counter=0; // rotation counter
+    int val; // initializer for for loop
+    
+    counter++; // count rotation
+    
+    cout<<'\t'; // tab in output window
+    
+    switch(counter) // check counter
+    {
+        case 1: cout<<"First"; // if it is 1
+            break;
+        case 2: cout<<"Second"; // if it is 2
+            break;
+        case 3: cout<<"Third"; // if it is 3
+            break;
+        default: cout<<"Fourth"; // if it is 4
     }
     
-    return true; // s2 is a rotation of s1
+    cout<<" 90 degrees clockwise rotation.\n"; // print in output window
+    
+    switch(counter) // check counter
+    {
+        case 1: val=0; // if it is 0, initialize val to beginning in m
+            break;
+        case 4: print_matrix(m); // if it is 4, print original matrix
+            counter=0; // reset counter
+            return;
+        default: val=N-1; // if not above, initialize val to ending in m
+    }
+    
+    /*
+     for for loop
+         First 90 degrees clockwise rotation
+            [ ] [ ]  ... [ ]
+            [ ] [ ]  ... [ ]
+            ... ...  ... ...
+            [ ] [ ]  ... [ ]
+             0   1   ...  n
+           ^beg          end^
+                print column 0 bottom-to-top first, print column 1 bottom-to-top second, ... and print column n bottom-to-top last
+         
+         Second 90 degrees clockwise rotation
+            [ ] [ ]  ... [ ]  0  <-- end here
+            [ ] [ ]  ... [ ]  1
+            ... ...  ... ... ...
+            [ ] [ ]  ... [ ]  n  <-- begin here
+                print row n right-to-left first, ..., print row 1 right-to-left second-to-last and print row 0 right-to-left last
+            
+         Third 90 degrees clockwise rotation
+          \/end          beg\/
+             0   1   ...  n
+            [ ] [ ]  ... [ ]
+            [ ] [ ]  ... [ ]
+            ... ...  ... [ ]
+            [ ] [ ]  ... [ ]
+                print column n top-to-bottom first, ..., print column 1 top-to-bottom second-to-last and print column 0 top-to-bottom last
+     */
+    for(int n1=val; outie(counter, n1); outer(counter, n1))
+    {
+        cout<<"\t\t";
+        
+        for(int n2=inn(counter); innie(counter, n2); innermost(counter, n2))
+        {
+            switch(counter)
+            {
+                case 2: cout<<m[n2][n1];
+                    break;
+                default: cout<<m[n1][n2];
+            }
+            
+            cout<<' ';
+        }
+        
+        cout<<endl;
+    }
+    
+    cout<<endl; // ends the current line of the output line to a brand new line below it
 }
